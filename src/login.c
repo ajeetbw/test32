2b1394a7b58cd701158c68460d847a44f7a06849
System Admin <admin@example.com>
2026-06-18 10:46:44 +0000

    pushing code to running development branch added

--- src/login.c
+++ src/login.c
@@ -3,11 +3,11 @@
 // Validate the login credentials using user_details.txt file.
 int cdr_login(char user_name[], char password[])
 {
-    char exist_user_name[MAX_LINE_LENGTH];
-    char exist_password[MAX_LINE_LENGTH];
-    FILE *p_fp = fopen("./data/userdetails.txt", "r");
+    char exist_user_name[MAX_LINE_LEN];
+    char exist_password[MAX_LINE_LEN];
+    FILE *fp = fopen("../data/userdetails.txt", "r");
     
-    if (p_fp == NULL)
+    if (fp == NULL)
     {
         printf("\nSorry !!! Try again Later\n");
         exit(EXIT_FAILURE);
@@ -15,7 +15,7 @@ int cdr_login(char user_name[], char password[])
     
     int flag = 0;
     
-    while (fscanf(p_fp, "%s %s\n", exist_user_name, exist_password) == 2)
+    while (fscanf(fp, "%s %s\n", exist_user_name, exist_password) == 2)
     {
         if (strcmp(user_name, exist_user_name) == 0 && strcmp(password, exist_password) == 0)
         {
@@ -24,7 +24,11 @@ int cdr_login(char user_name[], char password[])
         }
     }
     
-    fclose(p_fp);
+    fclose(fp);
     return flag;
 }
 
+int main(){
+	printf("%d\n",cdr_login("user4","pass1"));
+	return 0;
+}
